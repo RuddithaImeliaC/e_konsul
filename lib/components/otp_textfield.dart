093d85000cd5de79efb46921905ed920805e6d43
Kamal Firdaus <rakos94@yahoo.com>
2023-07-16 13:25:09 +0700

    move focus when otp insert

--- lib/components/otp_textfield.dart
+++ lib/components/otp_textfield.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class OtpField extends StatelessWidget {
+class OtpField extends StatefulWidget {
+  OtpField({super.key, required this.callback});
 
+  Function(String) callback;
+
+  @override
+  State<OtpField> createState() => _OtpFieldState();
+}
+
+class _OtpFieldState extends State<OtpField> {
   var otpController1 = TextEditingController();
   var otpController2 = TextEditingController();
   var otpController3 = TextEditingController();
   var otpController4 = TextEditingController();
   var otp;
-
-  Function(String) callback;
-
-  OtpField({super.key, required this.callback});
-
-  sendOtpToParent() {
-    otp = otpController1.text + otpController2.text + otpController3.text +
-        otpController4.text;
-    callback(otp);
-  }
+  FocusNode otpController1FocusNode = FocusNode();
+  FocusNode otpController2FocusNode = FocusNode();
+  FocusNode otpController3FocusNode = FocusNode();
+  FocusNode otpController4FocusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -49,8 +51,9 @@ class OtpField extends StatelessWidget {
           ],
           controller: otpController1,
           onChanged: (value) {
-            sendOtpToParent();
+            sendOtpToParent(context, value);
           },
+          focusNode: otpController1FocusNode,
         ),
       ),
       SizedBox(
@@ -71,8 +74,9 @@ class OtpField extends StatelessWidget {
           ],
           controller: otpController2,
           onChanged: (value) {
-            sendOtpToParent();
+            sendOtpToParent(context, value);
           },
+          focusNode: otpController2FocusNode,
         ),
       ),
       SizedBox(
@@ -93,8 +97,9 @@ class OtpField extends StatelessWidget {
           ],
           controller: otpController3,
           onChanged: (value) {
-            sendOtpToParent();
+            sendOtpToParent(context, value);
           },
+          focusNode: otpController3FocusNode,
         ),
       ),
       SizedBox(
@@ -115,12 +120,46 @@ class OtpField extends StatelessWidget {
           ],
           controller: otpController4,
           onChanged: (value) {
-            sendOtpToParent();
+            sendOtpToParent(context, value);
           },
+          focusNode: otpController4FocusNode,
         ),
       ),
       // SizedBox(height: 70),
       // MyButton(onTap: signUserIn)
     ]);
   }
+
+  sendOtpToParent(context, newValue) {
+    if(newValue != '') {
+      if (otpController1.text != '' && otpController2.text == '' &&
+          otpController3.text == '' && otpController4.text == '') {
+        FocusScope.of(context).requestFocus(otpController2FocusNode);
+      } else if (otpController1.text != '' && otpController2.text != '' &&
+          otpController3.text == '' && otpController4.text == '') {
+        FocusScope.of(context).requestFocus(otpController3FocusNode);
+      } else if (otpController1.text != '' && otpController2.text != '' &&
+          otpController3.text != '' && otpController4.text == '') {
+        FocusScope.of(context).requestFocus(otpController4FocusNode);
+      }
+    }
+
+    else if(newValue == '') {
+      if (otpController1.text != '' && otpController2.text != '' &&
+          otpController3.text != '' && otpController4.text == '') {
+        FocusScope.of(context).requestFocus(otpController3FocusNode);
+      } else if (otpController1.text != '' && otpController2.text != '' &&
+          otpController3.text == '' && otpController4.text == '') {
+        FocusScope.of(context).requestFocus(otpController2FocusNode);
+      } else if (otpController1.text != '' && otpController2.text == '' &&
+          otpController3.text == '' && otpController4.text == '') {
+        FocusScope.of(context).requestFocus(otpController1FocusNode);
+      }
+    }
+
+    otp = otpController1.text + otpController2.text + otpController3.text +
+        otpController4.text;
+    widget.callback(otp);
+  }
+
 }
